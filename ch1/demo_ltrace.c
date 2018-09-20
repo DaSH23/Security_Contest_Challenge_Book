a9f1d290bba09d7f1879235e32a48e78fa43fc85
苏翔宇 <su@suxiangyus-MacBook-Pro.local>
2018-09-20 15:01:46 +0900

    correct typo

--- ch1/demo_ltrace.c
+++ ch1/demo_ltrace.c
@@ -3,14 +3,14 @@
 
 int main() {
 	char buf[32];
-	char key[] = "d3m0_pr)gr4m_k3y";
+	char key[] = "d3m0_pr0gr4m_k3y";
 	
 	puts("Please input tha passphrase.");
 	fgets(buf, sizeof(buf), stdin);
 	strtok(buf, "\n");
 	
 	if (!strcmp(buf, key)) {
-		puts("COngratulations! Your flag is ctf4b{7h15_15_51mp13_ltrace}");
+		puts("Congratulations! Your flag is ctf4b{7h15_15_51mp13_ltrace}");
 	} else {
 		puts("Invalid inputs.");
 	}
